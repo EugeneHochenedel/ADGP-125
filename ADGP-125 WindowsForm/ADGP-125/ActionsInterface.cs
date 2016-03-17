9827fe9c3576f56b6f618817498c3521c9dcf6df
Eugene Hochenedel <eugeneatschool@gmail.com>
2016-03-17 11:57:51 -0500

    Added my FSM and began implementing Interfaces with a general outline.

---
+++ ADGP-125 WindowsForm/ADGP-125/ActionsInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADGP_125
+{
+	public interface ActionsInterface
+	{
+		int Attack(IStatsInterface Unit);
+		int Defend(IStatsInterface Self);
+		int Magic(IStatsInterface Current, IStatsInterface Unit);
+		bool Flee(IStatsInterface Running);
+		bool GameOver(IStatsInterface Dead);
+		bool Victory(IStatsInterface UnitCount);
+	}
+}
---
+++ ADGP-125 WindowsForm/ADGP-125/StatsInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADGP_125
+{
+	public interface IStatsInterface
+	{
+		int iHealth { get; set; }
+		int iMana { get; set; }
+		int iStrength { get; set; }
+		int iDefense { get; set; }
+		int iIntelligence { get; set; }
+		bool Alive { get; set; }
+	}
+}
